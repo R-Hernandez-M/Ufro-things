300aed227e72e412a1df157a0eec117424fe3e34
R-Hernandez-M <73892195+R-Hernandez-M@users.noreply.github.com>
2020-11-21 18:47:53 -0300

    Add files via upload

--- hola.java
+++ hola.java
@@ -1,6 +1,5 @@
 public class hola {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-        System.out.println("Mi nombre es Bruno Hernandez, bienvenidos");
+    public static void main(String[] args){
+        System.out.println("hola mundo");
     }
 }
