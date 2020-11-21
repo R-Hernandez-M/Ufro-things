e06455c2f7705a296d3187eb2ab8f8526bc913bf
R-Hernandez-M <73892195+R-Hernandez-M@users.noreply.github.com>
2020-11-21 18:03:29 -0300

    Add files via upload

---
+++ wenaloscabros.java
@@ -0,0 +1,5 @@
+public class wenaloscabros {
+    public static void main(String[] args){
+        System.out.println("hola mundo, o como lo diria yo Rodolfo Hernandez Montenegro, Wena los cabros");
+    }
+}
