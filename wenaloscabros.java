9e0ef1ecd34279eb9471c47eb4b82be0deeceaf2
R-Hernandez-M <73892195+R-Hernandez-M@users.noreply.github.com>
2020-11-21 18:33:35 -0300

    Update wenaloscabros.java

--- wenaloscabros.java
+++ wenaloscabros.java
@@ -1,5 +1,5 @@
 public class wenaloscabros {
     public static void main(String[] args){
-        System.out.println("hola mundo, o como lo diria yo Rodolfo Hernandez Montenegro, Wena los cabros");
+        System.out.println("hola mundo!!!");
     }
 }
